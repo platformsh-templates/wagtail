931a3edd2fd445dda490ea2a387412a98d31ff82
chadcarlson <chad.carlson@platform.sh>
2023-02-17 13:26:38 -0500

    Routes and allowed hosts.

--- myapp/settings/production.py
+++ myapp/settings/production.py
@@ -38,17 +38,28 @@ def decode(variable):
     except json.decoder.JSONDecodeError:
         print('Error decoding JSON, code %d', json.decoder.JSONDecodeError)
 
+python -c 'import os, json, base64; routes = json.loads(base64.b64decode(os.getenv("PLATFORM_ROUTES")).decode("utf-8")); print(routes); primary_route = [e for e in routes if e["primary"] == True]; print(primary_route)'
+
+primary_route = [e for e in routes if e['primary'] == True]; print(primary_route)
+
 # Import some Platform.sh settings from the environment.
 if (os.getenv('PLATFORM_APPLICATION_NAME') is not None):
+
     DEBUG = False
-    # WAGTAILADMIN_BASE_URL
-    # BASE_URL
+
+    ALLOWED_HOSTS = [
+        'localhost',
+        '127.0.0.1',
+        '.platformsh.site',
+    ]
+
     if (os.getenv('PLATFORM_APP_DIR') is not None):
         STATIC_ROOT = os.path.join(os.getenv('PLATFORM_APP_DIR'), 'static')
     if (os.getenv('PLATFORM_PROJECT_ENTROPY') is not None):
         SECRET_KEY = os.getenv('PLATFORM_PROJECT_ENTROPY')
-    # Database service configuration, post-build only.
+    
     if (os.getenv('PLATFORM_ENVIRONMENT') is not None):
+        # Database service configuration, post-build only.
         platformRelationships = decode(os.getenv('PLATFORM_RELATIONSHIPS'))
         db_settings = platformRelationships[PLATFORMSH_DB_RELATIONSHIP][0]
         DATABASES = {
@@ -65,3 +76,8 @@ if (os.getenv('PLATFORM_APPLICATION_NAME') is not None):
                 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
             }
         }
+        # Routes configuration, post-build only.
+        routes = decode(os.getenv('PLATFORM_ROUTES'))
+        primaryRoute = [route for route in routes.keys() if routes[route]["primary"] == True][0][:-1]
+        WAGTAILADMIN_BASE_URL = primaryRoute
+        BASE_URL = primaryRoute
